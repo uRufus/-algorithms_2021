85d89a77af0354ab1aea2ac4d9ca003792d7335d
Rufus <apdubrov@gmail.com>
2021-06-24 08:51:07 +0300

    ДЗ к 3 уроку

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -16,3 +16,18 @@
 р
 а
 """
+
+def unique_func(a):
+    new_list = []
+    string = hash(a)
+    while len(a) > 0:
+        for i in range(len(a)):
+            for b in range(len(a[i:])):
+                if string != hash(a[i:b + 1]) != hash("")and hash(a[i:b + 1]) not in new_list:
+                    new_list.append(hash(a[i:b + 1]))
+        a = a[1:]
+    return len(new_list)
+print(unique_func("papa"))
+
+
+
