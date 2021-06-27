863b105a61732275d263b5ee480f7da23c29675b
Rufus <apdubrov@gmail.com>
2021-06-28 00:08:02 +0300

    ДЗ к 4 уроку

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -17,8 +17,9 @@
 Укажите формулу сложности О-нотация каждого алгоритма
 и сделайте обоснование результатам.
 """
+import timeit
 
-
+# O(n ** 2)
 def simple(i):
     """Без использования «Решета Эратосфена»"""
     count = 1
@@ -41,3 +42,39 @@ def simple(i):
 
 i = int(input('Введите порядковый номер искомого простого числа: '))
 print(simple(i))
+
+
+# O(n ** 3)
+def complex(k):
+    n = k * 10
+    x = [i for i in range(2, n + 1)]
+    for i in x:
+        c = i * i
+        while c <= n * 10:
+            if c in x:
+                x.remove(c)
+            c += i
+
+    return x[k - 1]
+k = int(input('Введите порядковый номер искомого простого числа: '))
+print(complex(k))
+
+print("Сравнение скорости алгоритмов при искомом числе 10. Без решета")
+print(timeit.timeit("simple(10)", globals=globals(), number=10))
+print("Сравнение скорости алгоритмов при искомом числе 10. Через решето")
+print(timeit.timeit("complex(10)", globals=globals(), number=10))
+
+print("Сравнение скорости алгоритмов при искомом числе 100. Без решета")
+print(timeit.timeit("simple(100)", globals=globals(), number=10))
+print("Сравнение скорости алгоритмов при искомом числе 100. Через решето")
+print(timeit.timeit("complex(100)", globals=globals(), number=10))
+
+print("Сравнение скорости алгоритмов при искомом числе 1000. Без решета")
+print(timeit.timeit("simple(1000)", globals=globals(), number=10))
+print("Сравнение скорости алгоритмов при искомом числе 1000. Через решето")
+print(timeit.timeit("complex(1000)", globals=globals(), number=10))
+
+'''
+Функция решета эратосфена работает медленнее за счет большей сложности алгоритма, особенно это видно на большом 
+значении искомого числа
+'''
